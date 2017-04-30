e79d6c9c481ef3e81953ac315a9f874da5bc87cc
Akhilesh Kumar <akhileshsh026@hotmail.com>
2017-04-30 11:39:03 +0530

    Add project files.

---
+++ Amazon_Alexa_Challenge/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Amazon_Alexa_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t, n, k, Devloped  = 0, UnDevloped  = 0, min = 1001, a, b;
+            t=Convert.ToInt32(Console.ReadLine());
+            for ( int i1 = 0; i1 < t; i1++)
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                k = Convert.ToInt32(Console.ReadLine());
+                for (int j2 = 0; j2 < n; j2++)
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    if (a == 0)
+                        UnDevloped += b;
+                    else
+                    {
+                        if (min > b)
+                            min = b;
+                        Devloped  += b;
+                    }
+                }
+                if (min == 1001)
+                    min = 0;
+                else
+                    Devloped  -= min;
+                //printf("%d %d\n", Devloped  + UnDevloped , min);
+                Console.WriteLine("{0} {1}",Devloped + UnDevloped ,min);
+            }
+
+
+
+        }
+    }
+}
